b0e4c2a0a30fc47eff2ba0f195d803def93cd230
Cristiano Coelho Souza <ccoelho.me@gmail.com>
2020-07-28 00:15:31 -0300

    Projeto organizado

---
+++ Project/Business/IPersonProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Project
+{
+    public interface IPersonProcessor
+    {
+        (bool isValid, double heightInInches) ConvertHeightTextToInches(string heightText);
+        PersonModel CreatePerson(string firstName, string lastName, string heightText);
+        List<PersonModel> LoadPeople();
+        void SavePerson(PersonModel person);
+        void UpdatePerson(PersonModel person);
+    }
+}
