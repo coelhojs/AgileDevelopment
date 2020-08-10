21e334f7dc230844373d9b783b477bacedf319bf
Cristiano Coelho <ccoelho.me@gmail.com>
2020-08-10 09:22:16 -0300

    testes de exemplo

---
+++ UnitTests/FileReaderTests.cs
@@ -0,0 +1,23 @@
+﻿using Project;
+using System;
+using Xunit;
+
+namespace ProjectTests
+{
+    public class FileReaderTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = FileReader.ExampleLoadTextFile("This is a valid file name.");
+
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldFail()
+        {
+            Assert.Throws<ArgumentException>("file", () => FileReader.ExampleLoadTextFile(""));
+        }
+    }
+}
